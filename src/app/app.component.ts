b86b7f123d3e41be29532c3bdc8290e6edf2b0d7
MARTIN-JARVIS\mcj <m.c.j@live.co.uk>
2016-10-24 17:20:27 +0100

    POC-1: End to End

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {FlightType} from './flight-type.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
+
+  flightType = FlightType;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {SearchService} from './search.service';
+import {FindFlightService} from './find-flight.service';
+
 import { AppComponent } from './app.component';
+import { SearchPodComponent } from './search-pod/search-pod.component';
+import { FlightGridComponent } from './flight-grid/flight-grid.component';
+import { FlightTileComponent } from './flight-tile/flight-tile.component';
+import { SearchCriteriaDebugComponent } from './search-criteria-debug/search-criteria-debug.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPodComponent,
+    FlightGridComponent,
+    FlightTileComponent,
+    SearchCriteriaDebugComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [SearchService, FindFlightService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
